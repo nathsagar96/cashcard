852b05c0dc5080b8b79c8eebec4764b84f900484
nathsagar96 <nathsagar96@gmail.com>
2024-07-29 15:39:57 +0530

    LAB: Implementing PUT

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -3,7 +3,6 @@ package example.cashcard;
 import java.net.URI;
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,9 +22,9 @@ class CashCardController {
 
   @GetMapping("/{requestedId}")
   private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
-    Optional<CashCard> cashCardOptional = Optional.ofNullable(cashCardRepository.findByIdAndOwner(requestedId, principal.getName()));
-    if (cashCardOptional.isPresent()) {
-      return ResponseEntity.ok(cashCardOptional.get());
+    CashCard cashCard = findCashCard(requestedId, principal);
+    if (cashCard != null) {
+      return ResponseEntity.ok(cashCard);
     } else {
       return ResponseEntity.notFound().build();
     }
@@ -51,5 +50,20 @@ class CashCardController {
                     pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
             ));
     return ResponseEntity.ok(page.getContent());
+  }
+
+  @PutMapping("/{requestedId}")
+  private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+    CashCard cashCard = findCashCard(requestedId, principal);
+    if (null != cashCard) {
+      CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+      cashCardRepository.save(updatedCashCard);
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  private CashCard findCashCard(Long requestedId, Principal principal) {
+    return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
   }
 }
