3580cace3fa05cba6710a018e2bdbbdcf72351d6
nathsagar96 <nathsagar96@gmail.com>
2024-07-29 10:38:44 +0530

    LAB: Implementing GET

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CashCardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CashCardApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CashCardApplication.class, args);
+  }
 }
--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -11,33 +11,29 @@ import org.springframework.boot.test.json.JacksonTester;
 @JsonTest
 class CashCardJsonTest {
 
-  @Autowired
-  private JacksonTester<CashCard> json;
+  @Autowired private JacksonTester<CashCard> json;
 
   @Test
   void cashCardSerializationTest() throws IOException {
     CashCard cashCard = new CashCard(99L, 123.45);
     assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
     assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
-    assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
-            .isEqualTo(99);
+    assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
     assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
-    assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
-            .isEqualTo(123.45);
+    assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
   }
 
   @Test
   void cashCardDeserializationTest() throws IOException {
-    String expected = """
+    String expected =
+        """
                 {
                     "id":99,
                     "amount":123.45
                 }
                 """;
-    assertThat(json.parse(expected))
-            .isEqualTo(new CashCard(99L, 123.45));
+    assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
     assertThat(json.parseObject(expected).id()).isEqualTo(99);
     assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
   }
-
 }
